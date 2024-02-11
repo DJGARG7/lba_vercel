1beaea444f8c76ca3c08f6f38913ca3246430e46
DJGARG7 <18bce052@nirmauni.ac.in>
2024-02-11 15:34:07 +0530

    Improved error handling

--- lba-backend/controllers/fetchAllPublicData.js
+++ lba-backend/controllers/fetchAllPublicData.js
@@ -1,14 +1,19 @@
 const db = require("../config/db");
-const tablesMetadata = require("./tablesMetadata")
+const tablesMetadata = require("./tablesMetadata");
 const fetchAllPublicData = (req, res) => {
-
     const data = req.query;
     var query;
     var queryParam;
 
     //to check if request body has valid keys
     for (const key of tablesMetadata.keysToCheck) {
-        if (!(data.hasOwnProperty(key) && data[key] !== undefined && data[key] !== null)) {
+        if (
+            !(
+                data.hasOwnProperty(key) &&
+                data[key] !== undefined &&
+                data[key] !== null
+            )
+        ) {
             res.status(400).send("Invalid Request Body");
             return;
         }
@@ -17,7 +22,13 @@ const fetchAllPublicData = (req, res) => {
     //to check if data type is string
     const pub_table = data.pub_table;
     const pub_criteria = data.pub_criteria;
-    if (!(typeof pub_table === 'string' && typeof pub_criteria === 'string' && typeof data.pub_criteria_value === 'string')) {
+    if (
+        !(
+            typeof pub_table === "string" &&
+            typeof pub_criteria === "string" &&
+            typeof data.pub_criteria_value === "string"
+        )
+    ) {
         res.status(400).send("Invalid Request Body");
         return;
     }
@@ -34,33 +45,44 @@ const fetchAllPublicData = (req, res) => {
         return;
     }
 
-    if (pub_table === 'pub_lba_category' && pub_criteria === 'all') {
+    if (pub_table === "pub_lba_category" && pub_criteria === "all") {
         query = "SELECT * from lba_category where 1 = ?;";
         queryParam = [1];
-    } else if (pub_table === 'pub_lba_image' && pub_criteria === 'carousel_flag_Y') {
+    } else if (
+        pub_table === "pub_lba_image" &&
+        pub_criteria === "carousel_flag_Y"
+    ) {
         query = "SELECT * from lba_image where carousel_flag = ?;";
-        queryParam = ['Y'];
-    } else if (pub_table === 'pub_lba_product' && pub_criteria === 'keyword') {
-        query = "SELECT * from lba_product where product_deleted_flag = 'N' and lower(product_name) like ?;";
+        queryParam = ["Y"];
+    } else if (pub_table === "pub_lba_product" && pub_criteria === "keyword") {
+        query =
+            "SELECT * from lba_product where product_deleted_flag = 'N' and lower(product_name) like ?;";
         queryParam = [`%${data.pub_criteria_value.toLowerCase()}%`];
-    } else if (pub_table === 'pub_lba_product' && pub_criteria === 'category_id') {
-        query = "SELECT * from lba_product where product_deleted_flag = 'N' and category_id = ?;";
+    } else if (
+        pub_table === "pub_lba_product" &&
+        pub_criteria === "category_id"
+    ) {
+        query =
+            "SELECT * from lba_product where product_deleted_flag = 'N' and category_id = ?;";
         queryParam = [data.pub_criteria_value];
     } else {
-        query = "SELECT * from " + pub_table.substring(4) + " where " + pub_criteria + " = ?;";
+        query =
+            "SELECT * from " +
+            pub_table.substring(4) +
+            " where " +
+            pub_criteria +
+            " = ?;";
         queryParam = [data.pub_criteria_value];
     }
 
     try {
-        db.query(
-            query, queryParam,
-            (error, results) => {
-                if (error) throw error;
-                else res.send(results);
-            }
-        );
-    } catch (error) {
-        res.status(500).send(error);
+        db.query(query, queryParam, (error, results) => {
+            if (!error) res.send(results);
+            else res.status(500).send(error);
+        });
+    } catch (err) {
+        console.log("err", err);
+        res.status(500).send(err);
     }
-}
+};
 module.exports = fetchAllPublicData;
--- lba-backend/index.js
+++ lba-backend/index.js
@@ -11,17 +11,18 @@ const port = process.env.PORT || 3005;
 const frontendURL = process.env.FRONTEND || "http://localhost:3000"
 
 const app = express();
-app.use(express.static(path.join(__dirname,'build')));
+app.use(express.static(path.join(__dirname, "build")));
 app.use(
     cors({
-        origin: frontendURL,
-        credentials: true,
+        origin: function (origin, callback) {
+            callback(null, true);
+        },
     })
 );
 app.use(express.json());
-app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname,'build','index.html'));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.use("/public",publicRoute);
-app.use("/image",imageRoute);
+app.use("/public", publicRoute);
+app.use("/image", imageRoute);
 app.listen(port, () => console.log(`Backend Service running at port ${port}.`));
